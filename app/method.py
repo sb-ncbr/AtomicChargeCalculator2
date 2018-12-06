6eb8c65695a23589b5acc3509dd3a5a6cde4123f
Tomáš Raček <tom@krab1k.net>
2018-12-06 13:27:47 +0100

    Use parameter information from ChargeFW2

--- app/method.py
+++ app/method.py
@@ -1,7 +1,17 @@
+import os
 import json
+from collections import defaultdict
+
+from config import PARAMETERS_DIRECTORY
 
 with open('app/methods.json') as f:
     method_data = json.load(f)
 
-with open('app/parameters.json') as f:
-    parameter_data = json.load(f)
+parameter_data = defaultdict(list)
+for parameters in os.listdir(PARAMETERS_DIRECTORY):
+    with open(os.path.join(PARAMETERS_DIRECTORY, parameters)) as f:
+        p_data = json.load(f)
+        name = p_data['metadata']['name']
+        method = p_data['metadata']['method']
+        publication = p_data['metadata']['publication']
+        parameter_data[method].append({'name': name, 'publication': publication, 'filename': parameters})
