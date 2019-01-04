23abb0814bba9f20d23cefa86cc4d9f3f5cecc17
Tomáš Raček <tom@krab1k.net>
2019-01-04 15:57:53 +0100

    Remove some unnecessary whitespace in generated HTMLs.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from config import CONFIG_FILE
 
 application = Flask(__name__)
 
+application.jinja_env.trim_blocks = True
+application.jinja_env.lstrip_blocks = True
+
 with open(CONFIG_FILE) as f:
     application.config['SECRET_KEY'] = f.read().strip()
 
