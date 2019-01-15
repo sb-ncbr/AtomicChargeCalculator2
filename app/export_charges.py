9365dfb66bbd3ce4c59e2e93caf913bae6f8de72
Tomáš Raček <tom@krab1k.net>
2019-01-15 13:31:22 +0100

    Fix mol2 export when charges were not calculated.

--- app/export_charges.py
+++ app/export_charges.py
@@ -23,25 +23,48 @@ def prepare_mol2(tmpdir: str, method: str, parameters: Optional[str]):
         it = iter(structures.split('\n'))
         for line in it:
             if line == '@<TRIPOS>MOLECULE':
-                print(line, file=f)
+                name = next(it)
+                # Check if we have calculated charges for the molecule
+                if name in charges:
+                    print(line, file=f)
+                    print(name, file=f)
+                    charge_values = charges[name]
+                else:
+                    # Skip the whole record
+                    line = next(it)
+                    parts = line.split()
+                    atom_count, bond_count = int(parts[0]), int(parts[1])
+                    for _ in range(3):
+                        line = next(it)
+                    if line == '****':
+                        next(it)
+                    for _ in range(atom_count + bond_count + 2):
+                        next(it)
+                    continue
+
+                # Get counts and copy the line
                 line = next(it)
-                charge_values = charges[line]
                 print(line, file=f)
-                # Copy counts line
-                line = next(it)
-                print(line, file=f)
+                parts = line.split()
+                atom_count, bond_count = int(parts[0]), int(parts[1])
+                if atom_count != len(charge_values):
+                    raise ValueError('Charges don\'t match')
+
                 # Copy molecule type
                 line = next(it)
                 print(line, file=f)
+
                 # Replace GASTEIGER with USER_CHARGES
-                line = next(it)
+                next(it)
                 print('USER_CHARGES', file=f)
+
                 # Read optional stars delimiting and add comment
                 line = next(it)
                 if line == '****':
-                    line = next(it)
+                    next(it)
                 print('****', file=f)
                 print(f'Partial charges computed by ChargeCompute (method: {method}, parameters: {parameters})', file=f)
+
                 # Copy @<TRIPOS>ATOM
                 line = next(it)
                 print(line, file=f)
@@ -49,5 +72,10 @@ def prepare_mol2(tmpdir: str, method: str, parameters: Optional[str]):
                 for val in charge_values:
                     line = next(it)
                     print(f'{line[:-6]}{val:-6.3f}', file=f)
-            else:
+
+                # Copy @<TRIPOS>BOND
+                line = next(it)
                 print(line, file=f)
+                for _ in range(bond_count):
+                    line = next(it)
+                    print(line, file=f)
