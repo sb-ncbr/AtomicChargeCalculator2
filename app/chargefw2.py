452229359cfe6c3d30098463ade040f84f83e351
Tomáš Raček <tom@krab1k.net>
2019-09-04 15:44:46 +0200

    No need to add MKL path to ChargeFW as it's not required now

--- app/chargefw2.py
+++ app/chargefw2.py
@@ -3,18 +3,16 @@ import os
 from collections import Counter, defaultdict
 
 from .method import method_data
-from .config import MKL_PATH, PARAMETERS_DIRECTORY, CHARGEFW2_DIR
+from .config import PARAMETERS_DIRECTORY, CHARGEFW2_DIR
 
 
 def calculate(method_name, parameters_name, source, charge_out_dir):
     args = [os.path.join(CHARGEFW2_DIR, 'bin', 'chargefw2'), '--mode', 'charges', '--method', method_name,
             '--input-file', source, '--chg-out-dir', charge_out_dir, '--read-hetatm']
-    env = os.environ.copy()
-    env['LD_LIBRARY_PATH'] = MKL_PATH
     if next(m for m in method_data if m['internal_name'] == method_name)['has_parameters']:
         args.extend(['--par-file', os.path.join(PARAMETERS_DIRECTORY, parameters_name)])
 
-    calculation = subprocess.run(args, stderr=subprocess.PIPE, env=env, stdout=subprocess.PIPE)
+    calculation = subprocess.run(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     print(' '.join(calculation.args))
     return calculation
 
@@ -24,12 +22,9 @@ def get_suitable_methods(directory: str):
     for file in os.listdir(os.path.join(directory, 'input')):
         fullname = os.path.join(directory, 'input', file)
 
-        env = os.environ.copy()
-        env['LD_LIBRARY_PATH'] = MKL_PATH
-
         args = [os.path.join(CHARGEFW2_DIR, 'bin', 'chargefw2'), '--mode', 'suitable-methods', '--read-hetatm',
                 '--input-file', fullname]
-        calculation = subprocess.run(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE, env=env)
+        calculation = subprocess.run(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         print(' '.join(calculation.args))
         if calculation.returncode:
             print(calculation.stderr.decode('utf-8'))
