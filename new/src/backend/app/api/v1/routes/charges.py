d14bc8127c0cc855db269739ad89b36d3c33bece
Martin Pilát <48349161+pilatmartin@users.noreply.github.com>
2024-12-26 23:23:34 +0100

    feat: info, suitable methods, available params endpoints (#3)
    * feat: suitable methods and available params routes
    
    * feat: info route

--- new/src/backend/app/api/v1/routes/charges.py
+++ new/src/backend/app/api/v1/routes/charges.py
@@ -1,21 +1,42 @@
-import asyncio
 from fastapi import Depends, UploadFile
 from fastapi.routing import APIRouter
 from core.integrations.chargefw2.base import ChargeFW2Base
 from core.dependency_injection.container import Container
 from dependency_injector.wiring import inject, Provide
-from api.v1.schemas.response import ResponseMultiple
+from api.v1.schemas.response import ResponseMultiple, ResponseSingle
 from services.chargefw2 import ChargeFW2Service
 
 charges_router = APIRouter(prefix="/charges", tags=["charges"])
 
-queue = asyncio.Queue()
 
 @charges_router.get("/methods", tags=["charges", "methods"])
 @inject
 async def available_methods(chargefw2: ChargeFW2Base = Depends(Provide[Container.chargefw2])):
     methods: list[str] = chargefw2.get_available_methods()
     return ResponseMultiple(data=methods, total_count=len(methods), page_size=len(methods))
+
+
+@charges_router.post("/methods", tags=["charges", "methods"])
+@inject
+async def suitable_methods(
+    file: UploadFile, chargefw2: ChargeFW2Service = Depends(Provide[Container.chargefw2_service])
+):
+    methods: list[str] = await chargefw2.get_suitable_methods(file)
+    return ResponseMultiple(data=methods, total_count=len(methods), page_size=len(methods))
+
+
+@charges_router.get("/parameters/{method_name}", tags=["charges", "parameters"])
+@inject
+async def available_parameters(method_name: str, chargefw2: ChargeFW2Base = Depends(Provide[Container.chargefw2])):
+    parameters: list[str] = chargefw2.get_available_parameters(method_name)
+    return ResponseMultiple(data=parameters, total_count=len(parameters), page_size=len(parameters))
+
+
+@charges_router.post("/info", tags=["charges", "info"])
+@inject
+async def info(file: UploadFile, chargefw2: ChargeFW2Service = Depends(Provide[Container.chargefw2_service])):
+    info = await chargefw2.info(file)
+    return ResponseSingle(data=info)
 
 
 @charges_router.post(
@@ -30,4 +51,5 @@ async def calculate_charges(
     parameters_name: str | None = None,
     chargefw2: ChargeFW2Service = Depends(Provide[Container.chargefw2_service]),
 ):
-    return await chargefw2.calculate_charges(files, method_name, parameters_name)
+    charges = await chargefw2.calculate_charges(files, method_name, parameters_name)
+    return ResponseMultiple(data=charges, total_count=len(charges), page_size=len(charges))
--- new/src/backend/app/services/chargefw2.py
+++ new/src/backend/app/services/chargefw2.py
@@ -20,11 +20,28 @@ class ChargeFW2Service:
     def get_available_methods(self) -> list[str]:
         return self.chargefw2.get_available_methods()
 
+    async def get_suitable_methods(self, file: UploadFile) -> list[str]:
+        workdir = self.io.create_tmp_dir("suitable-methods")
+        new_file_path = await self.io.store_upload_file(file, workdir)
+        molecules = await self.read_molecules(new_file_path)
+
+        self.logger.info(f"Getting suitable methods for file {file.filename}")
+        suitable_methods = self.chargefw2.get_suitable_methods(molecules)
+        self.logger.info(f"Successfully got suitable methods for file {file.filename}")
+
+        return suitable_methods
+
     def get_available_parameters(self, method: str) -> list[str]:
         return self.chargefw2.get_available_parameters(method)
 
-    def read_molecules(self, file_path: str) -> Molecules:
-        return self.chargefw2.molecules(file_path)
+    async def read_molecules(self, file_path: str) -> Molecules:
+        loop = asyncio.get_event_loop()
+
+        self.logger.info(f"Loading molecules from file {file_path}.")
+        molecules = await loop.run_in_executor(self.executor, self.chargefw2.molecules, file_path)
+        self.logger.info(f"Successfully loaded molecules from file {file_path}.")
+
+        return molecules
 
     async def calculate_charges(
         self, files: list[UploadFile], method_name: str, parameters_name: str | None = None
@@ -37,7 +54,7 @@ class ChargeFW2Service:
                 self.logger.info(f"Calculating charges for file {file.filename}.")
                 loop = asyncio.get_event_loop()
 
-                molecules = await loop.run_in_executor(self.executor, self.read_molecules, new_file_path)
+                molecules = await self.read_molecules(new_file_path)
                 charges = await loop.run_in_executor(
                     self.executor, self.chargefw2.calculate_charges, molecules, method_name, parameters_name
                 )
@@ -50,3 +67,16 @@ class ChargeFW2Service:
         # Process all files concurrently
         results = await asyncio.gather(*[process_file(file) for file in files])
         return results
+
+    async def info(self, file: UploadFile):
+        workdir = self.io.create_tmp_dir("info")
+        new_file_path = await self.io.store_upload_file(file, workdir)
+        try:
+            self.logger.info(f"Getting info for file {file.filename}.")
+            molecules = await self.read_molecules(new_file_path)
+            self.logger.info(f"Successfully got info for file {file.filename}.")
+
+            return molecules.info().to_dict()
+        except Exception as e:
+            self.logger.error(f"Error getting info for file {file.filename}: {e}")
+            return {"file": file.filename, "error": str(e)}
