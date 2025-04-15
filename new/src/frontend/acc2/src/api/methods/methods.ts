43edf2f60bdb60c6ea712513f3d74080e09dbd4e
Martin Pilát <48349161+pilatmartin@users.noreply.github.com>
2025-04-15 22:29:29 +0200

    fix: removed dummy from frontend (#42)

--- new/src/frontend/acc2/src/api/methods/methods.ts
+++ new/src/frontend/acc2/src/api/methods/methods.ts
@@ -13,5 +13,14 @@ export const getSuitableMethods = async (
     throw new Error(response.data.message);
   }
 
-  return response.data.data;
+  const data = response.data.data;
+
+  // we don't want to show 'dummy' method on frontend
+  const dummyKey = "dummy";
+  data.methods = data.methods.filter(
+    (method) => method.internalName !== dummyKey
+  );
+  delete data.parameters[dummyKey];
+
+  return data;
 };
